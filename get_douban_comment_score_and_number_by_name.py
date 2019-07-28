1e3cc30a1ab3a8e7f94226eb4eea33edaa2f4038
xoaybc <lxh1031138448@gmail.com>
2019-07-29 01:39:42 +0800

    add year compare

--- get_douban_comment_score_and_number_by_name.py
+++ get_douban_comment_score_and_number_by_name.py
@@ -31,51 +31,53 @@ def search_movie(movie_name,movie_year):
     r = requests.get(url_link, headers=douban_headers)
     douban_headers['Cookie'] = r.cookies
     soup = BeautifulSoup(r.text.encode('utf-8'), 'lxml')
-    all_subuject_result = soup.find_all(attrs={'class' : 'result'})[0:1]
+    all_subuject_result = soup.find_all(attrs={'class' : 'result'})[0:3]
     data = {}
+    data['name'] = movie_name
+    data['chn_title'] = 'Not found in douban'
+    data['link'] = 'N/A'
+    data['subject_id'] = 'N/A'
+    data['year'] = 'N/A'
+    data['rating_score'] = 'N/A'
+    data['rating_total_nums'] = 'N/A'
     if all_subuject_result:
         for result in all_subuject_result:
             #print result
             title = result.find_all(attrs={'class' : 'title'})[0]
-            rating_info = result.find_all(attrs={'class' : 'rating-info'})[0]
-            # movie title info
-            chn_title = title.h3.a.text.strip()
-            link = title.h3.a['href']
-            link_decode = unquote(link)
-            link = re.match(r'^.*url=(.*?)&query=.*$', link_decode).group(1)
-            subject_id = re.match(r'^.*url=(https://movie.douban.com/subject/)?([0-9]+)/&query=.*$', link_decode).group(2)
             year = title.find_all(attrs={'class' : 'subject-cast'})[0].text.split('原名:')[1].split('/')[-1].replace(' ','')
-            # movie rating info
-            rating_total_nums = rating_info.find_all('span', attrs={"class": None})[0].text
-            rating_total_nums = re.sub(r'(\(|\))', '', rating_total_nums)
-            if rating_total_nums and '暂无评分' not in rating_total_nums and '尚未' not in rating_total_nums:
-                rating_score = rating_info.find_all('span', attrs={"class": "rating_nums"})[0].text
+            year_minus = abs(int(movie_year)-int(year))
+            if year_minus <= 2:
+                rating_info = result.find_all(attrs={'class' : 'rating-info'})[0]
+                # movie title info
+                chn_title = title.h3.a.text.strip()
+                link = title.h3.a['href']
+                link_decode = unquote(link)
+                link = re.match(r'^.*url=(.*?)&query=.*$', link_decode).group(1)
+                subject_id = re.match(r'^.*url=(https://movie.douban.com/subject/)?([0-9]+)/&query=.*$', link_decode).group(2)
+                # movie rating info
+                rating_total_nums = rating_info.find_all('span', attrs={"class": None})[0].text
+                rating_total_nums = re.sub(r'(\(|\))', '', rating_total_nums)
+                if rating_total_nums and '暂无评分' not in rating_total_nums and '尚未' not in rating_total_nums:
+                    rating_score = rating_info.find_all('span', attrs={"class": "rating_nums"})[0].text
+                else:
+                    rating_score = rating_total_nums
+                #print "o-year:{0} m-year:{1}" .format(year,movie_year)
+                data['name'] = movie_name
+                data['chn_title'] = chn_title
+                data['link'] = link
+                data['subject_id'] = subject_id
+                data['year'] = year
+                data['rating_score'] = rating_score
+                data['rating_total_nums'] = rating_total_nums
             else:
-                rating_score = rating_total_nums
-            #print "o-year:{0} m-year:{1}" .format(year,movie_year)
-            data['name'] = movie_name
-            data['chn_title'] = chn_title
-            data['link'] = link
-            data['subject_id'] = subject_id
-            data['year'] = year
-            data['rating_score'] = rating_score
-            data['rating_total_nums'] = rating_total_nums
-            #print movie_name,original_name_format,movie_name_new
-    else:
-        data['name'] = movie_name
-        data['chn_title'] = 'Not found in douban'
-        data['link'] = 'N/A'
-        data['subject_id'] = 'N/A'
-        data['year'] = 'N/A'
-        data['rating_score'] = 'N/A'
-        data['rating_total_nums'] = 'N/A'
+                continue
     #print data
     return data
 
 with open('movie.name','rU') as f:
     for line in f:
         movie_info = line.strip()
-        p = re.compile(r'^(.*)(\.|\s)([0-9]{4})\.?.*$')
+        p = re.compile(r'^(.*)[\.| |\s]?([0-9]{4})\.?.*$')
         match_obj = p.match(movie_info)
         if match_obj is not None:
             movie_name = match_obj.group(1)
