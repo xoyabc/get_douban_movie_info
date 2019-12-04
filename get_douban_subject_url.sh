c2e5f493c58ae1f1d0c3d0273d4b9fac4a079da4
xoyabc <1031138448@qq.com>
2019-12-04 15:50:34 +0800

    update douban new tag url

--- get_douban_subject_url.sh
+++ get_douban_subject_url.sh
@@ -7,6 +7,7 @@ for i in $(seq 0 25 100)
 do
     # douban tag
     # curl -s "https://movie.douban.com/tag/2018?start=${i}&type=T" -A  'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' |pup '.nbg attr{href}' >> douban_subjects
+    # https://movie.douban.com/j/new_search_subjects?sort=T&range=0,10&tags=2019&start=0
     # dou list
     curl -s "https://www.douban.com/doulist/${doulist_id}/?start=${i}&sort=seq&playable=0&sub_type=" -A 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' |pup '[class="post"] a attr{href}' |awk -F "/" '{print $(NF-1)}' >> douban_subjects
     sleep 1
